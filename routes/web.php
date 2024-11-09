25ec751397ec12daf480609ca88f57001f0ae565
GAZDA-EDU <gazda.edu@gmail.com>
2024-11-09 13:32:38 +0100

    Admin Dashboard -2

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,17 @@ Route::get('/', function () {
 });
 
 Route::middleware('admin')->group(function(){
-    Route::get('index', [UserController::class, 'index'])->name('user');
-    Route::post('store', [UserController::class, 'store'])->name('new_user');
-    Route::get('create', [UserController::class, 'create'])->name('user.create');
-    Route::get('user-delete/{id}', [UserController::class, 'destroy'])->name('user.delete');
-    Route::apiResource('business', BusinessController::class)->name('index', 'businesses');
+    Route::get('user',[UserController::class,'index'])->name('user');
+    Route::post('store',[UserController::class,'store'])->name('new_user');
+    Route::get('user-create',[UserController::class,'create'])->name('user.create');
+    Route::get('user-delete/{id}',[UserController::class,'destroy'])->name('user.delete');
+
+    Route::get('businesses',[BusinessController::class,'index'])->name('businesses');
+    Route::post('business-store',[BusinessController::class,'store'])->name('new_business');
+    Route::get('business-create',[BusinessController::class,'create'])->name('business.create');
+    Route::get('business-delete/{id}',[BusinessController::class,'destroy'])->name('business.delete');
 });
 
-Route::post('admin_login',[Admin_loginController::class, 'login'])->name('admin_login');
-Route::get('showloginform',[Admin_loginController::class, 'showloginform'])->name('login_form');
-Route::get('logout',[Admin_loginController::class, 'logout'])->name('logout');
+Route::post('admin_login',[Admin_loginController::class,'login'])->name('admin_login');
+Route::get('showloginform',[Admin_loginController::class,'showloginform'])->name('login_form');
+Route::get('logout',[Admin_loginController::class,'logout'])->name('logout');
